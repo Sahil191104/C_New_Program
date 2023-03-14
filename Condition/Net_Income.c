33f92a0535f1f5dce2a085c79cd75184c4cec805
Sahil191104 <sahilrupareliya95@gmail.com>
2023-03-14 20:13:53 +0545

    14_3_2023

---
+++ Condition/Net_Income.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main() {
+	int income, expenses, net_income, Total_net_income, Tax;
+	
+	printf("Please enter your Income:-");
+	scanf("%d", &income);
+	
+	printf("Please enter your Expenses:-");
+	scanf("%d", &expenses);
+	
+	net_income = income - expenses;
+	
+	if (net_income >= 0 && net_income <= 10000) {
+		printf("\nNot to Pay any Tax\n");
+	} else if (net_income >= 10000 && net_income <= 15000) {
+		Tax = net_income * 0.10;
+		Total_net_income = net_income - Tax;
+	} else if (net_income > 15000) {
+		Tax = net_income * 0.18;
+		Total_net_income = net_income - Tax;
+	}
+	
+	printf("Net Income is:- %d\n",net_income);
+	printf("Tax is:- %d\n",Tax);
+	printf("Total Net Income is:- %d\n",Total_net_income);
+	
+	return 0;
+}
