aa5033d70451aa33dcf00bcf580a72ea0f6b1f66
Sahil191104 <sahilrupareliya95@gmail.com>
2023-03-15 18:48:52 +0545

    nfg

--- Condition/Net_Income.c
+++ Condition/Net_Income.c
@@ -14,11 +14,11 @@ int main() {
 	if (net_income >= 0 && net_income <= 10000) {
 		printf("\nNot to Pay any Tax\n");
 	} else if (net_income >= 10000 && net_income <= 15000) {
-		Tax = net_income * 0.10;
-		Total_net_income = net_income - Tax;
+//		Tax = net_income * 0.10;
+		Total_net_income = net_income - (Tax= net_income * 0.10);
 	} else if (net_income > 15000) {
-		Tax = net_income * 0.18;
-		Total_net_income = net_income - Tax;
+//		Tax = net_income * 0.18;
+		Total_net_income = net_income - (Tax=net_income * 0.18);
 	}
 	
 	printf("Net Income is:- %d\n",net_income);
---
+++ Condition/Scholrship.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+	int cast_Criteria, Grade_Criteria, Grade, income, Total_scholarship, scholarship;
+	
+	printf("Please Enter your Income:-");
+	scanf("%d", &income);
+	
+	if (cast_Criteria) {
+		if (open) {
+			printf("No Scholarship");
+		} else if (obc) {
+			Total_scholarship = income + (scholarship=income * 0.25);
+		} else if (sc) {
+			Total_scholarship = income + (scholarship=income * 0.50);
+		} else if (st) {
+			Total_scholarship = income + (scholarship=income * 1);
+		}
+	} else {
+		
+	}
+	
+	return 0;
+}
