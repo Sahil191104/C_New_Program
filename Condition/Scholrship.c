d26161a1e4ae26a17b2be69233d1137fc51e51ad
Sahil191104 <sahilrupareliya95@gmail.com>
2023-03-18 22:42:48 +0545

    19

--- Condition/Scholrship.c
+++ Condition/Scholrship.c
@@ -1,24 +1,39 @@
 #include<stdio.h>
 
 int main() {
-	int cast_Criteria, Grade_Criteria, Grade, income, Total_scholarship, scholarship;
-	
-	printf("Please Enter your Income:-");
-	scanf("%d", &income);
-	
-	if (cast_Criteria) {
-		if (open) {
-			printf("No Scholarship");
-		} else if (obc) {
-			Total_scholarship = income + (scholarship=income * 0.25);
-		} else if (sc) {
-			Total_scholarship = income + (scholarship=income * 0.50);
-		} else if (st) {
-			Total_scholarship = income + (scholarship=income * 1);
-		}
-	} else {
-		
+	char cast, grade;
+	int fees, scholarship, totale_fees;
+	float cgpa;
+
+	printf("Enter your cast, CGPA and Fees: ");
+	scanf("%c %f %d", &cast, &cgpa, &fees);
+
+	if (cgpa > 9 && cgpa <= 10) {
+		grade = 'A';
+	} else if (cgpa > 8.5 && cgpa <= 9) {
+		grade = 'B';
+	} else if (cgpa > 8 && cgpa <= 8.5) {
+		grade = 'C';
+	} else if (cgpa > 7.5 && cgpa <= 8) {
+		grade = 'D';
 	}
+
+	if (cast == 'O') {
+		printf("No Sorry Scholarship: %c", cast);
+	} else if (cast == 'B' && grade >= 'B') {
+		scholarship = fees * 0.25;
+	} else if (cast == 'S' && grade >= 'B') {
+		scholarship = fees * 0.50;
+	} else if (cast == 'T' && grade >= 'B') {
+		scholarship = fees * 1;
+	}
+
+	totale_fees = fees - scholarship;
 	
+	printf("Your Fees is:- %d\n", fees);
+	printf("Your CGPA is:- %f\n", cgpa);
+	printf("Your scholarship is:- %d\n", scholarship);
+	printf("Your Totale fees is:- %d\n", totale_fees);
+
 	return 0;
 }
---
+++ Condition/Scholrship_Nested_if.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+	char cast, grade;
+	int fees, scholarship, totale_fees;
+	float cgpa;
+	
+	printf();printf("Enter your cast, CGPA and Fees: ");
+	scanf("%c %f %d", &cast, &cgpa, &fees);
+	
+	if () {
+		
+	} else {
+		
+	}
+	
+	return 0;
+}
