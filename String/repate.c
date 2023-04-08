37735b5a9c60c463495d4b7982190da5559d3da1
Sudip-Gajera <sudipgajera1997@gmail.com>
2023-04-08 08:53:36 +0530

    8_4_2023

--- String/repate.c
+++ String/repate.c
@@ -11,7 +11,7 @@ int main() {
 		for (j=i+1; str[j] != '\0'; j++) {
 			if (str[i] == str[j]) {
 				for (k=j; str[k] != '\0'; k++) {
-					str[k] == str[k+1];
+					str[k] = str[k+1];
 				}
 			}
 		}
